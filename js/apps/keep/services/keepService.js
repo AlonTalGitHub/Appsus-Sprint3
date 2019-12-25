3dcd32a0a03b2254eb322acfe69e430ca4852e4e
shay rosenthal <shayke.rosenthal@gmail.com>
2019-12-25 19:18:44 +0200

    wednsday1918

--- js/apps/keep/services/keepService.js
+++ js/apps/keep/services/keepService.js
@@ -4,7 +4,7 @@ const NOTES_KEY='notes'
 function store(key, any) {
     localStorage[key] = JSON.stringify(any);
 }
-
+var gNotes=[];
 function load(key) {
     var str = localStorage[key] || 'null';
     return JSON.parse(str);
@@ -15,19 +15,24 @@ function createNote() {
     return {
         type: 'Text',
         isPinned: false,
-        info: {},
+        info: {
+            url:null,
+            title:null,
+            text:getRandomText()
+        },
         timeStamp: (new Date).getTime()
     }
 }
+
 function getRandomText(){
-    var numOfWords=Math.floor(Math.random()*10);
+    var numOfWords=Math.floor(1+Math.random()*10);
     var randText='';
     var loremStr='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, eveniet quasi dolore sunt consequuntur cumque! Atque cumque eos sint temporibus amet voluptatem officia iusto quod vero quos exercitationem, iure animi!'
     for (var i=0;i<numOfWords;i++){
-        randText+=loremStr.split('')
+        randText+=loremStr.split(' ')[i]+' ';
     }
+return randText;
 }
-
 function createNotes(numOfNotes) {
     var notes = [];
     if (!load(NOTES_KEY)) {
@@ -37,6 +42,10 @@ function createNotes(numOfNotes) {
         store(NOTES_KEY, notes)
     }
     else notes = load(NOTES_KEY);
+    gNotes=notes;
     return notes;
+}
 
+function getNotes(){
+    return Promise.resolve(gNotes);
 }
